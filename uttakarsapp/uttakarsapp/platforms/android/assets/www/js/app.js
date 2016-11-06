af0ce469442f1df5ccaaac2fcd1d27c327eb24c4
uttakarsh <romeomohansinha@yahoo.com>
2016-11-07 02:12:45 +0530

    Change in controller

--- uttakarsapp/uttakarsapp/platforms/android/assets/www/js/app.js
+++ uttakarsapp/uttakarsapp/platforms/android/assets/www/js/app.js
@@ -35,4 +35,31 @@ angular.module('starter', ['ionic'])
             alert(reason + "");
         })
     };
+    $scope.data = {
+        speechText: ''
+    };
+    $scope.recognizedText = '';
+
+    $scope.speakText = function () {
+        TTS.speak({
+            text: $scope.data.speechText,
+            locale: 'en-GB',
+            rate: 1.5
+        }, function () {
+            // Do Something after success
+        }, function (reason) {
+            // Handle the error case
+        });
+    };
+
+    $scope.record = function () {
+        var recognition = new SpeechRecognition();
+        recognition.onresult = function (event) {
+            if (event.results.length > 0) {
+                $scope.recognizedText = event.results[0][0].transcript;
+                $scope.$apply()
+            }
+        };
+        recognition.start();
+    };
 })
